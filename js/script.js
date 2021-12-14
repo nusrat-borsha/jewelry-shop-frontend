eb08835affeeb7598520ad1f6b861680543d8325
nusrat-borsha <nuameeen@gmail.com>
2021-12-14 13:46:52 +0600

    add modal window for login and sign up

--- js/script.js
+++ js/script.js
@@ -69,3 +69,37 @@ const slider = function(){
     };
     
     slider();
+
+//==========MODAL WINDOW=========
+const modal = document.querySelector('.modal-container');
+const overlay = document.querySelector('.overlay');
+const heroImg = document.querySelector('.hero-img-box');
+const heroSect = document.querySelector('.hero-text-box');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  heroImg.classList.add('overlay');
+  heroSect.classList.add('overlay');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  heroImg.classList.remove('overlay');
+  heroSect.classList.remove('overlay');
+
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  // console.log(e.key);
+
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+}); 
